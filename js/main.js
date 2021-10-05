8f7fc1e1b04ff21d213e811f3b65b02d64c67a50
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2021-10-05 12:05:43 +0300

    Настройка объекта

--- js/main.js
+++ js/main.js
@@ -1,21 +1,115 @@
 // math.floor - округление
 
-function getRandomNumber(min, max) {
-  if (max > min && min >= 0) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  } else {
-    return false;
-  }
+// function getRandomNumber(min, max) {
+//   if (max > min && min >= 0) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function getRandomNumberCoordination(min, max, numberSymbol) {
+//   if (max > min && min >= 0 && numberSymbol >= 0) {
+//     return (Math.random() * (max - min) + min).toFixed(numberSymbol);
+//   } else {
+//     return false;
+//   }
+// }
+
+function getRandomPositiveFloat(a, b, digits = 1) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return result.toFixed(digits);
 }
 
-getRandomNumber(1, 100);
-
-function getRandomNumberCoordination(min, max, numberSymbol) {
-  if (max > min && min >= 0 && numberSymbol >= 0) {
-    return (Math.random() * (max - min + 1) + min).toFixed(numberSymbol);
-  } else {
-    return false;
-  }
+function getRandomPositiveInteger(a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
 
-getRandomNumberCoordination(10, 100, 3);
+const TITLE = [
+  "у моря",
+  "не у моря",
+  "у кекса",
+  "не у кекса",
+  "дом у дороги",
+  "дом не у дороги",
+  "дом который построил кекс",
+  "дом,который не строил кекс",
+];
+
+const TYPE = ["palace", "flat", "house", "bungalow", "hotel"];
+const CHECKIN = ["12:00", "13:00", "14:00"];
+const CHECKOUT = ["12:00", "13:00", "14:00"];
+const FEATURES = [
+  "wifi",
+  "dishwasher",
+  "parking",
+  "washer",
+  "elevator",
+  "conditioner",
+];
+const DESCRIPTION = [
+  "чудесное место",
+  "я бы жил",
+  "я живу",
+  "посоветовал бы всем друзьям",
+  "не жил,но хвалю",
+];
+
+const PHOTOS = [
+  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg",
+  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg",
+  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg",
+];
+
+const COORDINATIONX = getRandomPositiveFloat(35.65, 35.7, 5);
+const COORDINATIONY = getRandomPositiveFloat(139.7, 139.8, 5);
+
+// Аватар (но числа повторяются...)
+function getAvatar() {
+  const avatarIndex = getRandomPositiveInteger(1, 10);
+  return `img/avatars/user${avatarIndex}.png`;
+}
+
+const getRandomArrayElement = (elements) =>
+  elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+// массив случайной длины без повторяющихся строк
+
+const getRandomArray = (elements) =>
+  elements
+    .sort(() => Math.random() - Math.random())
+    .slice(0, getRandomPositiveInteger(0, elements.length - 1));
+
+const createHotel = () => {
+  return {
+    author: {
+      avatar: getAvatar(),
+    },
+    offer: {
+      title: getRandomArrayElement(TITLE),
+      address: `${COORDINATIONX} , ${COORDINATIONY}`,
+      price: getRandomPositiveInteger(1000, 9000),
+      type: getRandomArrayElement(TYPE),
+      rooms: getRandomPositiveInteger(1, 10),
+      guests: getRandomPositiveInteger(1, 40),
+      checkin: getRandomArrayElement(CHECKIN),
+      checkout: getRandomArrayElement(CHECKOUT),
+      description: getRandomArrayElement(DESCRIPTION),
+      features: getRandomArray(FEATURES),
+      photos: getRandomArray(PHOTOS),
+    },
+    location: {
+      lat: COORDINATIONX,
+      lng: COORDINATIONY,
+    },
+  };
+};
+
+const similarHotel = Array.from({ length: 10 }, createHotel);
+
+console.log(similarHotel);

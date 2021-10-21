3c5400cab9afae8ccfe4130b46ce713d92d449fc
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2021-10-21 13:47:27 +0300

    Отрисуй меня полностью (часть 2)

---
+++ js/state.js
@@ -0,0 +1,28 @@
+const adForm = document.querySelector(".ad-form");
+const mapFilters = document.querySelector(".map__filters");
+const adFormFields = Array.from(adForm.querySelectorAll("fieldset"));
+const mapFiltersFields = Array.from(mapFilters.children);
+
+const useNonActiveState = () => {
+  adForm.classList.add("ad-form--disabled");
+  mapFilters.classList.add("map__filters--disabled");
+  adFormFields.forEach((item) => {
+    item.disabled = true;
+  });
+  mapFiltersFields.forEach((item) => {
+    item.disabled = true;
+  });
+};
+
+const useActiveState = () => {
+  adForm.classList.remove("ad-form--disabled");
+  mapFilters.classList.remove("map__filters--disabled");
+  adFormFields.forEach((item) => {
+    item.disabled = false;
+  });
+  mapFiltersFields.forEach((item) => {
+    item.disabled = false;
+  });
+};
+
+export { useNonActiveState, useActiveState };
